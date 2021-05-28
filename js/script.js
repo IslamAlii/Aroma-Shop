454bc08cd94532ab9f796aa1efa4f7ea0240908e
Islam Ali <Admin@User-PC>
2021-05-28 22:09:03 +0200

    Uploading the project files

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector(".btn-nav").addEventListener("click", () => {
+  document.querySelector("header nav").classList.toggle("active");
+  document.querySelector(".btn-nav").classList.toggle("active");
+});
